4038e9b9b25910ffd5aa05abbfed5deff2b8f6e7
Rudi Grinberg <me@rgrinberg.com>
2022-06-18 13:18:25 -0500

    refactor(dap): use rpc's sexp encoding
    avoids the need to write serialize/deserialize functions manually
    
    also remove the whole serializable thing. the serialization format is
    already decided by rpc to be sexp.
    
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>
    
    ps-id: df9a3e11-4e14-4da5-8f3b-07321f9aef95

--- otherlibs/action-plugin/src/sexpable_intf.ml
+++ otherlibs/action-plugin/src/sexpable_intf.ml
@@ -1,9 +1,44 @@
-open Stdune
+open Import
+
+module Deserialization_error = struct
+  type t =
+    | Version_mismatch of int
+    | Parse_error
+end
 
 module type Sexpable = sig
   type t
 
-  val sexp_of_t : t -> Sexp.t
+  val to_sexp : t -> Sexp.t
 
-  val t_of_sexp : Sexp.t -> t Option.t
+  val of_sexp : Sexp.t -> (t, Deserialization_error.t) result
+end
+
+module type S = sig
+  type t
+
+  val conv : t Conv.value
+
+  val version : int
+end
+
+module Make (TypeToSerialize : S) = struct
+  open TypeToSerialize
+
+  let conv =
+    let open Conv in
+    pair int conv
+
+  let of_sexp sexp =
+    match Conv.of_sexp Conv.(pair int sexp) ~version:(0, 0) sexp with
+    | Error _ -> Error Deserialization_error.Parse_error
+    | Ok (version, sexp) -> (
+      match Int.equal version TypeToSerialize.version with
+      | false -> Error (Deserialization_error.Version_mismatch version)
+      | true -> (
+        match Conv.of_sexp TypeToSerialize.conv ~version:(0, 0) sexp with
+        | Error _ -> Error Deserialization_error.Parse_error
+        | Ok v -> Ok v))
+
+  let to_sexp t = Conv.to_sexp conv (TypeToSerialize.version, t)
 end
